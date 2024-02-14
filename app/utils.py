a1504184ba2d971b442245924229fe9bd4856e5b
Cristian Valencia <cevm.12293@hotmail.com>
2024-02-14 16:29:24 -0600

    add function to read txt files and return it content

--- app/utils.py
+++ app/utils.py
@@ -7,3 +7,8 @@ def move_and_rename_file(source_path, destination_directory, new_name):
     # Move and rename the file
     shutil.move(source_path, destination_path)
 
+def read_txt_file(file_path):
+    with open(file_path,'r') as file:
+        data = file.read()
+        
+    return data
