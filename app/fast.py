9c716440ae20d5e99d06f78b3d6d8b89622480e0
Cristian Valencia <cevm.12293@hotmail.com>
2024-02-14 00:00:29 -0600

    add comments and import test lib

--- app/fast.py
+++ app/fast.py
@@ -1,10 +1,9 @@
 
 from typing import Union
 import datetime
-#import databasefuncs
 from pydantic import BaseModel
-
 from fastapi import FastAPI
+import test
 
 app = FastAPI()
 
@@ -17,3 +16,16 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/translate")
+def translate_file(filename: str):
+    # PENDING TO ADD FUNCTION TO TRANSLATION
+    # 1 - Get the full path of the .wav or .ogg file
+    # 2 - Ask for transcription with test.lets_transcribe(file_path, file_format)
+    # 3 - Return the transcription
+    
+    
+    # Translate the content
+    translated_text = "DUMMY TEXT"
+
+    return {"filename": filename, "transcription": translated_text}
