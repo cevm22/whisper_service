1eef89877d97b9b234a21519a80c285d5dbcb2e5
Cristian Valencia <cevm.12293@hotmail.com>
2024-02-14 16:29:59 -0600

    implement return transcription data and move the txt file into bucket

--- app/fast.py
+++ app/fast.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import test
 import utils
+import re
 
 app = FastAPI()
 
@@ -26,10 +27,18 @@ def translate_file(filename: str):
     file_path = str(bucket_path) + str(filename) 
     # 2 - Ask for transcription with test.lets_transcribe(file_path, "txt")
     res = test.lets_transcribe(file_path, "txt")
-    # 3 - Return the transcription
     
+    # 3 - get transcription data
+    # path of transcription .txt
+    transcription_path = res[1][0]
+    transcription_data = utils.read_txt_file(f'./{transcription_path}')
     
-    # Translate the content
-    translated_text = "DUMMY TEXT"
-
-    return {"filename": filename, "transcription": res, "filepath":file_path}
+    # 4 - get file name without extension after dot
+    match = re.match(r'.*/([^/.]+)\.', file_path)
+    only_file_name = match.group(1)
+    
+    # 5 - move txt file to bucket
+    utils.move_and_rename_file(f'./{transcription_path}', f'{bucket_path}', f'{only_file_name}.txt')
+    
+    # 6 - Return the transcription
+    return {"filename": filename, "transcription": transcription_data, "audio_filepath":file_path}
